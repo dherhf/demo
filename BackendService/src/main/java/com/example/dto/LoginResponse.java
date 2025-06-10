8ca08090df732f3d0ca98fb16a3d72bccac066e5
dherhf <dherhf@outlook.com>
2025-06-10 21:27:07 +0800

    完成用户登录控制器

---
+++ BackendService/src/main/java/com/example/dto/LoginResponse.java
@@ -0,0 +1,28 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponse {
+
+    private String userId;
+    private String username;
+    private String token; // 如果使用JWT或其他token
+    @Builder.Default
+    private String  loginTime = LocalDate.now().toString();
+
+    // 自定义构造函数，确保loginTime自动设置
+    public LoginResponse(String userId, String username) {
+        this.userId = userId;
+        this.username = username;
+        this.loginTime = LocalDate.now().toString();
+    }
+}
