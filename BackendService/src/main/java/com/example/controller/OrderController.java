b6a6aeec373fa316c9363332e787ee4877b73870
dherhf <dherhf@outlook.com>
2025-06-14 11:58:21 +0800

    新增 OrderController 类，设置 API 路径和跨域支持

---
+++ BackendService/src/main/java/com/example/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/order")
+@CrossOrigin(origins = "*")
+public class OrderController {
+}
